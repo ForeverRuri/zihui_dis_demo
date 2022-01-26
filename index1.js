8165485f8d53ea641449cbe8bcf46562b21ed7fe
ForeverRuri <525153411@qq.com>
2022-01-26 20:33:30 +0800

    vv

---
+++ index1.js
@@ -0,0 +1,89 @@
+import * as T from './snowpack/pkg/three.js'
+import { OrbitControls } from "./snowpack/pkg/three/examples/jsm/controls/OrbitControls.js";
+import { TrackballControls } from "./snowpack/pkg/three/examples/jsm/controls/TrackballControls.js";
+
+class product {
+    constructor(id, width, height) {
+        this.id = id
+        this.width = width
+        this.height = height
+        this.init()
+    }
+
+
+    init() {
+        this.add_scene()
+        this.add_light()
+        this.add_camera()
+        this.add_cube()
+        this.add_renderer()
+        this.add_controls()
+        this.tick()
+    }
+
+    add_scene() {
+        this.scene = new T.Scene()
+
+    }
+
+    add_renderer() {
+        this.canvas = document.getElementById(this.id)
+        this.renderer = new T.WebGL1Renderer({
+            canvas: this.canvas
+        })
+        this.renderer.setSize(this.width, this.height)
+        this.renderer.render(this.scene, this.camera)
+        // 修改网页颜色
+        this.renderer.setClearColor("#eeefef")
+    }
+
+
+    add_camera() {
+        this.camera = new T.PerspectiveCamera(55, this.width / this.height, 1, 1000)
+        this.camera.position.z = 5
+        this.scene.add(this.camera)
+    }
+
+    add_cube() {
+        this.cubeGeo = new T.BoxBufferGeometry(1.06, 2.710, 0.658)
+        var loader = new T.TextureLoader();
+        loader.setPath('./images/视络疏六面图/');
+        var mats = [
+            '蓝A3.jpg', '蓝A5.jpg',
+            '蓝A6.jpg', '蓝A1.jpg',
+            '蓝A2.jpg', '蓝A4.jpg'
+        ].map(pic => {
+            return new T.MeshLambertMaterial({ map: loader.load(pic) ,color:"#ffffff"});
+        });
+
+        this.cube = new T.Mesh(this.cubeGeo, mats)
+        this.scene.add(this.cube)
+    }
+
+    add_light() {
+        this.light = new T.AmbientLight(0xffffff, 1)
+        this.scene.add(this.light)
+    }
+    add_controls() {
+        this.controls = new TrackballControls(this.camera, this.canvas)
+        // this.controls = new OrbitControls(this.camera, this.canvas)
+        this.controls.rotateSpeed=3
+        // this.controls.noPan=true
+        this.controls.update()
+    }
+
+    tick = () => {
+        requestAnimationFrame(this.tick)
+
+        // this.cube.rotation.y += 0.01
+        this.controls.update()
+        this.renderer.render(this.scene, this.camera)
+
+    }
+
+
+
+}
+
+
+new product("webgl", innerWidth, innerHeight)
